3435530387ad42e67f1eef14624af60b680ec04b
Aleksandr Babkin <bubkin-007@ya.ru>
2025-09-05 23:42:32 +0300

    refactor client and server

--- WhatsAppClient/WhatsAppClient.cs
+++ WhatsAppClient/WhatsAppClient.cs
@@ -1,74 +1,72 @@
-﻿
-using System;
 using System.Reflection;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 
-public class WhatsAppWeb
-{
-    public Dictionary<Guid, WhatsAppWeb> _WhatsAppWebSessions;
-    private Guid _sessionId;
-    public bool _logged = false;
-    private readonly IWebDriver _driver;
-    private string _base64image;
-    private readonly string _whatsappURL = "https://web.whatsapp.com/;";
-    private readonly string _FolderPathToStoreSession;
-    private readonly int _qrcoderefreshtime = 55;
-    public readonly string messageurl = "https://web.whatsapp.com/send/?phone={destanation_number]}&text={text}&type=phone_number&app_absent=0";
-    private readonly long phonenumber;
-    private bool _QrCodeReady;
-    private readonly int process;
+namespace WhatsAppClient;
 
-    public WhatsAppWeb()
+public class WhatsAppClient
+{
+    public Dictionary<Guid, WhatsAppClient> Sessions { get; }
+    private readonly Guid _sessionId;
+    public bool Logged { get; private set; }
+    private readonly IWebDriver _driver;
+    private string _qrCode;
+    private readonly string _whatsAppUrl = "https://web.whatsapp.com/";
+    private readonly string _sessionFolder;
+    public readonly string MessageUrl = "https://web.whatsapp.com/send/?phone={destination_number}&text={text}&type=phone_number&app_absent=0";
+    private bool _qrCodeReady;
+
+    public WhatsAppClient()
     {
-        _WhatsAppWebSessions = new();
+        Sessions = new Dictionary<Guid, WhatsAppClient>();
         SessionLoader();
     }
 
-    public WhatsAppWeb(Guid sessionId, WebDriver driver)
+    public WhatsAppClient(Guid sessionId, WebDriver driver)
     {
         _sessionId = sessionId;
         _driver = driver;
-        CheckPhone();
+        CheckPhone().Wait();
     }
-    public WhatsAppWeb(Guid sessionId)
+
+    public WhatsAppClient(Guid sessionId)
     {
         _sessionId = sessionId;
         var assemblyDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        _FolderPathToStoreSession = assemblyDirectory + "/sessionfolder" + $"/{sessionId}";
-        Directory.CreateDirectory(_FolderPathToStoreSession);
+        _sessionFolder = Path.Combine(assemblyDirectory!, "sessionfolder", sessionId.ToString());
+        Directory.CreateDirectory(_sessionFolder);
         ChromeOptions option = new();
-         option.AddArguments("--headless");
-        option.AddArgument("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36");  //last PC Chrome
-        option.AddArgument("--user-data-dir=" + _FolderPathToStoreSession);
+        option.AddArguments("--headless");
+        option.AddArgument("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36");
+        option.AddArgument("--user-data-dir=" + _sessionFolder);
         _driver = new ChromeDriver(option);
-        _driver.Navigate().GoToUrl(_whatsappURL + "/");
-        bool wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(60)).Until(d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
-        _QrCodeReady = false;
-        CheckQrCodeExistanse();
-        CheckPhone();
+        _driver.Navigate().GoToUrl(_whatsAppUrl);
+        new WebDriverWait(_driver, TimeSpan.FromSeconds(60)).Until(d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
+        _qrCodeReady = false;
+        CheckQrCodeExistance().Wait();
+        CheckPhone().Wait();
     }
 
     void SessionLoader()
     {
         var assemblyDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        var sessionlist = Directory.GetDirectories(assemblyDirectory + "/sessionfolder");
-        foreach (var session in sessionlist)
+        var sessionList = Directory.GetDirectories(Path.Combine(assemblyDirectory!, "sessionfolder"));
+        foreach (var session in sessionList)
         {
             var dir = new DirectoryInfo(session).Name;
-            var sessionid = Guid.Parse(dir);
-            var driver = new WhatsAppWeb(sessionid);
-            _WhatsAppWebSessions.Add(Guid.Parse(dir), driver);
+            var sessionId = Guid.Parse(dir);
+            var driver = new WhatsAppClient(sessionId);
+            Sessions.Add(sessionId, driver);
         }
     }
-    public async void Status()
-    {
 
-        //qr and phone need to be paralel 
+    public async Task StatusAsync()
+    {
         await CheckPhone();
-        await CheckQrCodeExistanse();
+        await CheckQrCodeExistance();
     }
+
     public void Exit()
     {
         _driver.Close();
@@ -81,71 +79,71 @@ public class WhatsAppWeb
             try
             {
                 var wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 10));
-                var canvaselement = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.ClassName("selectable-text")));
-                _logged = true;
-                canvaselement.Click();
-                Task.Delay(1000).Wait();
-                canvaselement.Clear();
-                Task.Delay(1000).Wait();
-                canvaselement.Click();
-                Task.Delay(1000).Wait();
-                canvaselement.SendKeys("You");
-                var you = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.CssSelector("div[aria-label='Search results.']")));
-                Task.Delay(2000).Wait();
+                var canvasElement = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.ClassName("selectable-text")));
+                Logged = true;
+                canvasElement.Click();
+                await Task.Delay(1000);
+                canvasElement.Clear();
+                await Task.Delay(1000);
+                canvasElement.Click();
+                await Task.Delay(1000);
+                canvasElement.SendKeys("You");
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.CssSelector("div[aria-label='Search results.']")));
+                await Task.Delay(2000);
                 var span = _driver.FindElements(By.CssSelector("span[aria-label='']"));
                 foreach (var element in span)
                 {
                     if (element.Text.Contains('+'))
                     {
-                        _logged = true;
+                        Logged = true;
                         var phone = element.Text;
-                    };
+                    }
                 }
             }
-            catch (Exception ex)
+            catch
             {
-
             }
-        } while (!_logged);
+        } while (!Logged);
     }
+
     private async Task RequestQrCode()
     {
-        if (_base64image != null)
+        if (_qrCode != null)
         {
             return;
         }
-        //qrexpired?
         var wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 10));
-        var canvaselement = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.TagName("canvas")));
+        var canvasElement = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.TagName("canvas")));
         var toDataURLscript = "return arguments[0].toDataURL('image/png');";
         IJavaScriptExecutor js = (IJavaScriptExecutor)_driver;
-        var freshbase64image = (string)js.ExecuteScript(toDataURLscript, canvaselement);
-        if (_base64image != freshbase64image)
+        var freshBase64Image = (string)js.ExecuteScript(toDataURLscript, canvasElement);
+        if (_qrCode != freshBase64Image)
         {
-            _base64image = freshbase64image;
+            _qrCode = freshBase64Image;
         }
     }
-    private async Task CheckQrCodeExistanse()
+
+    private async Task CheckQrCodeExistance()
     {
         do
         {
             try
             {
                 var wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 10));
-                var canvaselement = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.TagName("canvas")));
-                _QrCodeReady = true;
-                _logged = false;
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.TagName("canvas")));
+                _qrCodeReady = true;
+                Logged = false;
                 return;
             }
-            catch (Exception ex)
+            catch
             {
                 Screenshot ss = ((ITakesScreenshot)_driver).GetScreenshot();
-                var time  = DateTime.Now.ToString("hh:mm:ss_dd.MM.yyyy");
-                //check folder here 
-                ss.SaveAsFile(Path.Combine(_FolderPathToStoreSession,"debug",$"Image{DateTime.Now.ToString("hh:mm:ss_dd.MM.yyyy")}.png"));
+                var time = DateTime.Now.ToString("hh:mm:ss_dd.MM.yyyy");
+                ss.SaveAsFile(Path.Combine(_sessionFolder, "debug", $"Image{time}.png"));
             }
-        } while (!_QrCodeReady);
+        } while (!_qrCodeReady);
     }
-    public string GetQrCodeImage => $@"<!DOCTYPE html><html><body><img src='{_base64image}'/></body></html>";
+
+    public string GetQrCodeImage => $"<!DOCTYPE html><html><body><img src='{_qrCode}'/></body></html>";
 }
 
--- WhatsAppWeb/APIKeyCheckMiddleware.cs
+++ WhatsAppWeb/APIKeyCheckMiddleware.cs
@@ -11,26 +11,22 @@ internal class APIKeyCheckMiddleware
 
     public async Task InvokeAsync(HttpContext httpContext)
     {    
-        //we could inject here our database context to do checks against the db
         if (httpContext.Request.Headers.TryGetValue("X-CUSTOM-HEADER", out StringValues value))
         {
-            //do the checks on key
             var apikey = value;
         }
         else
         {
-            //return 403
             httpContext.Response.StatusCode = 403;
-        }   
+        }
         await _next(httpContext);
     }
 }
-
-// Extension method used to add the middleware to the HTTP request pipeline.
 public static class APIKeyCheckMiddlewareExtensions
 {
     public static IApplicationBuilder UseAPIKeyCheckMiddleware(this IApplicationBuilder builder)
-    { 
+    {
         return builder.UseMiddleware<APIKeyCheckMiddleware>();
     }
 }
+
